31b33154e37e2e12ed65e2274d55115cf9ea4b73
Taj <tajmarie92@gmail.com>
2022-06-07 21:00:51 -0500

    Add project files.

---
+++ LearningMethods/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace LearningMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string GetHello()
+        {
+            // delcaring a method
+            string sentence = "Hello World!";
+            return sentence;
+        }
+
+        // declaring a method
+        static double GetHello2()
+            
+        {
+            double sentence = 3.15;
+            return sentence;
+            
+        }
+
+        //declaring a method
+        static decimal GetHello3()
+        {
+            decimal sentence = 3.14m;
+            return sentence;
+        }
+
+        //declaring a method
+        static int GetHello4()
+        {
+            int sentence = 15;
+            return sentence;
+               
+        }
+
+        //declaring a method
+        static bool GetHello5()
+        {
+            bool sentence = true;
+            return sentence;
+        }
+
+        //delcaring a method
+        static string GetHello6()
+        {
+            string sentence = "Hi";
+            return sentence;
+        }
+
+        //declaring a method
+        static double GetHello7()
+        {
+            double sentence= 1546.5;
+            return sentence;
+
+        }
+         // declaring a method
+         static decimal GetHello8()
+        {
+            decimal sentence = 354.12m;
+            return sentence;
+
+        }
+
+        //delcaring a method
+        static string GetHello9()
+        {
+            string sentence = "Bye";
+            return sentence;
+        }
+
+        //declaring a method
+        static bool GetHello10()
+        {
+            bool sentence = false;
+            return sentence;
+        }
+
+        //declaring a method
+        static double GetHello11()
+        {
+            double sentence = 54.26;
+            return sentence;
+        }
+
+        //declaring a method
+
+        static decimal GetHello11()
+        {
+            decimal sentence = 365.12m;
+            return sentence;
+        }
+
+        //declaring a method
+
+        static bool GetHello12()
+        {
+            bool sentence = true;
+            return sentence;
+
+        }
+        //declaring a method
+        static string GetHello13()
+        {
+            string sentence = "Yasss";
+            return sentence;
+        }
+
+        //declaring a method
+        static int GetHello14()
+        {
+            int sentence = 19;
+            return sentence;
+        }
+
+        //declaring a method
+        static bool GetHello15()
+        {
+            bool sentence = false;
+            return sentence;
+        }
+
+        //declaring a method
+        static string GetHello16()
+        {
+            string sentence = "Bye Bye";
+            return sentence;
+        }
+
+        //declaring a method
+        static decimal GetHello17()
+        {
+            decimal sentence = 471.26m;
+            return sentence;
+        }
+
+        //declaring a method
+
+        static double GetHello18()
+        {
+            double sentence = 5412.3;
+            return sentence;
+        }
+
+        //declaring a method
+
+        static int GetHello19()
+        {
+            int sentence = 29;
+            return sentence;
+        }
+
+        //declaring a method
+
+        static string GetHello20()
+        {
+            string sentence = "Done!";
+            return sentence;
+        }
+    }
+}
